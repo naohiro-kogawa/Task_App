66a03e4ebdbb35fb821a9f433c39302dee5f0133
EC2 Default User <ec2-user@ip-172-31-37-84.us-east-2.compute.internal>
2019-09-01 10:46:58 +0000

    ヘッダー右側のリストB7.3トップページview&プロフィールページIDカラム作成後

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20190829144852) do
+ActiveRecord::Schema.define(version: 20190831073346) do
 
   create_table "users", force: :cascade do |t|
     t.string "name"
@@ -20,6 +20,7 @@ ActiveRecord::Schema.define(version: 20190829144852) do
     t.string "password_digest"
     t.string "remember_digest"
     t.boolean "admin"
+    t.integer "user_id"
     t.index ["email"], name: "index_users_on_email", unique: true
   end
 
