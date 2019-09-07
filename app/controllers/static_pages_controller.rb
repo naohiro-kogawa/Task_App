e157c405de9a04589e253b897cb420a32a8720fd
EC2 Default User <ec2-user@ip-172-31-37-84.us-east-2.compute.internal>
2019-09-07 06:35:41 +0000

    ヘッダー右側のリストB7.5ヘッダーリスト、ログイン状態ボタン、フラッシュ設定途中

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,11 +1,14 @@
 class StaticPagesController < ApplicationController
+
   def top
   end
   
   def guest_admin
+     flash.now[:success] = 'ゲスト管理者としてログインしました。'
   end
   
   def guest_user
+     flash.now[:success] = 'ゲストユーザーとしてログインしました。'
   end
   
 end
