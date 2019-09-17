b9e72a075e411fc09dd0b1cb9d536f3dde970905
EC2 Default User <ec2-user@ip-172-31-37-84.us-east-2.compute.internal>
2019-09-17 14:51:50 +0000

    新規タスク作成ページview：コントローラーまで：次回route,一部日本語化はから

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,8 +1,13 @@
 class TasksController < ApplicationController
   
   
-  def index    
-    @user = User.find(params[:user_id])
+  def index    # タスク一覧
+    @user = User.find(params[:user_id]) #認可機能の編集でドライを統一する。
     @tasks = @user.tasks
+  end
+  
+  def new     # タスク新規作成ページ
+    @user = User.find(params[:user_id])
+    @task = Task.new
   end
 end
