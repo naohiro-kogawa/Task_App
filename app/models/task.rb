a2875a1460b533c532470af01ec5cf84eb873127
EC2 Default User <ec2-user@ip-172-31-37-84.us-east-2.compute.internal>
2019-09-29 07:26:09 +0000

    C10：タスクモデルにバリデーション設定

--- app/models/task.rb
+++ app/models/task.rb
@@ -1,3 +1,9 @@
 class Task < ApplicationRecord
+  default_scope -> { order(created_at: :desc) } #降順
+  
   belongs_to :user
+  
+  validates :user_id, presence: true
+  validates :name, presence: true, length: { maximum: 50 }
+  validates :description, presence: true, length: { in: 5..300 }
 end
