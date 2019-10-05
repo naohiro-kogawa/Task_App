03e1c24e5d460d7961704ee0a4d0d2f9c44ad19d
EC2 Default User <ec2-user@ip-172-31-37-84.us-east-2.compute.internal>
2019-10-05 05:39:16 +0000

    D456:サンプルユーザー：一覧ページ：ページネーション：管理者は削除ボタンあり

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,14 @@ User.create!( name: "管理者",
               password: "password",
               password_confirmation: "password",
               admin: true)
+              
+99.times do |n|
+  name  = Faker::Name.name
+  email = "sample-#{n+1}@email.com"
+  password = "password"
+  User.create!(name: name,
+               email: email,
+               password: password,
+               password_confirmation: password)
+end
+
